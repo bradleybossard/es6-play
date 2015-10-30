b0b7d703a6bdbfa89f574a41ee6ab74df530b897
Bradley Bossard <bradleybossard@gmail.com>
2015-10-30 22:08:36 +0000

    Adding arrow function notation example

---
+++ arrows.js
@@ -0,0 +1,24 @@
+var evens = [2, 4, 6, 8];
+var fives = [];
+
+// Expression bodies
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
+
+// Statement bodies
+nums.forEach(v => {
+  if (v % 5 === 0)
+    fives.push(v);
+});
+
+console.log(fives);
+
+// Lexical this
+var bob = {
+  _name: "Bob",
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+};
