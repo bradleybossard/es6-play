c3e91454bf3fba1af8f3e60d62c5ed9622f75b65
Bradley Bossard <bradleybossard@gmail.com>
2015-11-02 17:00:43 +0000

    Adding  destructuring

---
+++ destructuring.js
@@ -0,0 +1,29 @@
+// This file does not work as destructuring is not yet supported in Node v4
+
+// list matching
+var [a, ,b] = [1,2,3];
+a === 1;
+b === 3;
+
+
+// object matching
+var { op: a, lhs: { op: b }, rhs: c }
+       = getASTNode()
+
+// object matching shorthand
+// binds `op`, `lhs` and `rhs` in scope
+var {op, lhs, rhs} = getASTNode()
+
+// Can be used in parameter position
+function g({name: x}) {
+  console.log(x);
+}
+g({name: 5})
+
+// Fail-soft destructuring
+var [a] = [];
+a === undefined;
+
+// Fail-soft destructuring with defaults
+var [a = 1] = [];
+a === 1;
